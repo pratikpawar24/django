8e35ff348f71da1919dc8b765f323b586e7ec97c
Pratik Pawar <pratikpawrpune@gmail.com>
2025-03-19 21:27:42 +0530

    Second Commit

--- flipkart/electronics/views.py
+++ flipkart/electronics/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 def home(request):
@@ -7,4 +8,11 @@ def home(request):
 
 def product_details(request):
     prod = Product.objects.all()
-    return render(request, 'electronics/product_details.html',{"prod" : prod})
+    form = ProductForm()
+    
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request,'electronics/product_details.html',{"prod":prod,'form':form})
+
